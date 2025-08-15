176d14681eda50b14fbb6a913bc69934d6554766
VolodymyrV1 <162920898+VolodymyrV1@users.noreply.github.com>
2025-08-15 12:28:52 +0000

    Deploying to gh-pages from @ VolodymyrV1/goit-js-hw-09@5e59d51dcca066d70346800b88e7869bf198aa58 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/2-form-B5Qt9EMX.js";/* empty css                      */console.log("gallery");
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/2-form-B5Qt9EMX.js";/* empty css                      */console.log("Form");
+//# sourceMappingURL=2-form.js.map
