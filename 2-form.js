9c6412dbdf88e276d3a0ae5304a3186cb0743412
VolodymyrV1 <162920898+VolodymyrV1@users.noreply.github.com>
2025-08-18 14:23:51 +0000

    Deploying to gh-pages from @ VolodymyrV1/goit-js-hw-09@a537574e959b7c008d9edd589452020b846dd73b 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/2-form-B5Qt9EMX.js";/* empty css                      */console.log("Form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Form");
 //# sourceMappingURL=2-form.js.map
